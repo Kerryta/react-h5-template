3e683db0df19d3ff4f04244e5d6bd48e9aa017b8
kerryta <1105067295@qq.com>
2023-07-12 17:22:26 +0800

    feat: add postcss-px-to-viewport-ts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,21 @@
 import { Button } from "antd-mobile/2x"
+import styled from "styled-components"
 
 const Home = () => {
   return (
-    <>
+    <HomeStyled>
       <Button
         color='primary'
         block
       >
         Home
       </Button>
-    </>
+    </HomeStyled>
   )
 }
+
+const HomeStyled = styled.div`
+  padding: 10px;
+`
 
 export default Home
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
+import postcsspxtoviewport from "postcss-px-to-viewport-ts"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -26,6 +27,29 @@ export default defineConfig(({ command, mode }) => {
           secure: false,
           rewrite: path => path.replace("/api", "/"),
         },
+      },
+    },
+    css: {
+      postcss: {
+        plugins: [
+          postcsspxtoviewport({
+            unitToConvert: "px",
+            viewportWidth: 375,
+            unitPrecision: 5,
+            propList: ["*"],
+            viewportUnit: "vw",
+            fontViewportUnit: "vw",
+            selectorBlackList: [],
+            minPixelValue: 1,
+            mediaQuery: false,
+            replace: true,
+            exclude: undefined,
+            include: undefined,
+            landscape: false,
+            landscapeUnit: "vw",
+            landscapeWidth: 568,
+          }),
+        ],
       },
     },
     build: {
