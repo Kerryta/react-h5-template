97ef2dd59a4c077cf22571c52408e31824ab3d1c
kerryta <1105067295@qq.com>
2023-07-12 16:00:43 +0800

    build: configuration vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  return {
+    base: "./",
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    server: {
+      host: true,
+      port: 3361,
+      open: false,
+      cors: true,
+      proxy: {
+        "/api": {
+          target: "https://development-service",
+          changeOrigin: true,
+          secure: false,
+          rewrite: path => path.replace("/api", "/"),
+        },
+      },
+    },
+    build: {
+      minify: "esbuild",
+      outDir: "dist",
+      assetsDir: "assets",
+      chunkSizeWarningLimit: 1000,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vandor: ["react"],
+          },
+        },
+      },
+    },
+    esbuild: {
+      drop: env.ENV !== "development" ? ["console", "debugger"] : [],
+    },
+  }
 })
